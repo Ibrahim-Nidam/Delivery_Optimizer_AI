53a60e1d46422035ac29a22ebafa615cb45f55b3
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-07 17:03:19 +0100

    feat(service) : closetour method added

--- src/main/java/com/deliveryoptimizer/service/interfaces/TourService.java
+++ src/main/java/com/deliveryoptimizer/service/interfaces/TourService.java
@@ -13,4 +13,5 @@ public interface TourService {
     void deleteTour(Long id);
     TourDTO addDeliveriesToTour(Long tourId, List<Long> deliveryIds);
     List<Long> optimizeTour(Long tourId);
+    TourDTO closeTour(Long id);
 }
